d52dfc560aa34dd725dcc2741aa50fb47ce9e672
Srinivash <98004952+srinivash-01@users.noreply.github.com>
2024-04-07 09:35:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ mongoose
   });
 
 const app = express();
-
+app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
