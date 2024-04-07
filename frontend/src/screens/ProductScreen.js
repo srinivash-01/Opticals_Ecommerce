b8cb8924c8e85d575f4f1c3530a4d348a244920b
Srinivash <98004952+srinivash-01@users.noreply.github.com>
2024-04-07 10:37:45 +0530

    Update ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -91,7 +91,7 @@ function ProductScreen() {
     const fetchData = async () => {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
-        const result = await axios.get(`/api/products/slug/${slug}`);
+        const result = await axios.get(`https://opticals-ecommerce.vercel.app/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
